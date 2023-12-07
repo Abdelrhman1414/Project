a2c714e576a25feab5dabd44ef8ead370c3d3397
Mohamedyasser02 <mohammedyasser0031@gmail.com>
2023-12-08 00:56:42 +0200

    created product apis

---
+++ Backend/models/productmodel.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema({
+// id_: {
+//     type:Number,
+//     required:true,
+//     unique:true
+// },
+
+title : {
+    type: String,
+    required: true,
+    trim: true
+
+},
+category : {
+    required: true,
+    type: String,
+},
+description : {
+    type: String,
+    required: true,
+},
+price : {
+    type: Number,
+    required: true,
+},
+countInst: {
+    type: Number,
+    required : true
+},
+images: {
+    type: String
+},
+reviews: {
+    type: String,
+   // postedby: {type: mongoose.Schema.Types.ObjectId, ref: "User"},
+}
+
+
+
+
+}, {timestamps: true});
+
+module.exports = mongoose.model("Product",productSchema);

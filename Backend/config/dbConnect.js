1e2b986ff65998c2dee14b1d66be640d4442e754
Abdelrhman1414 <10812021100142@stud.cu.edu.eg>
2023-12-08 14:56:55 +0200

    last

---
+++ Backend/config/dbConnect.js
@@ -0,0 +1,12 @@
+const { default: mongoose } = require("mongoose")
+
+const dbConnect = () =>{
+try{
+    const conn=mongoose.connect("mongodb://localhost:27017/Project");
+console.log("Database connected Succesfully")
+}
+catch(error){
+   console.log(" Data Error")
+}
+}
+module.exports= dbConnect;

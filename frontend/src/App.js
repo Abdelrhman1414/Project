31409449f5b2ff634316dd315339de1aff919219
MohamedMatter1 <153040153+MohamedMatter1@users.noreply.github.com>
2023-12-08 05:04:49 +0200

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,62 @@
-import './App.css';
-import HomePage from './component/home.js';
-import React from 'react';
-import './App.css';
-import Products from './component/Products.js';
-import About from './component/About.js';
-import ContactUs from './component/ContactUs.js';
-import ItemDetail from './component/ItemDetail.js';
-import * as ROUTES from "./constants/routes"
+import "./App.css";
+import SignIn from "./components/auth/SignIn";
+import SignUp from "./components/auth/SignUp";
+import Home from "./components/Landing/Home";
+import Users from "./components/auth/Users";
+// import Login from "./components/Login.js";
+import Cart from "./components/Cart.js";
+import './components/Profile.css';
+// import './components/Cart.css';
+import React, { useState } from 'react';
+import Reviews from "./components/Reviews.js";
+import * as ROUTES from "./constants/routes";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-const App = () => {
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Profile from "./components/Profile.js";
+import AddToCart from "./components/Cart.js";
+import SearchComponent from "./components/SearchComponent.js";
+const theme = createTheme();
+
+function App() {
+  const [searchResults, setSearchResults] = useState([]);
+  const allData = Users;
+  const handleSearch = (results) => {
+    setSearchResults(results);
+  };
   return (
-<>
-    <Router>
-      <Routes>
-        <Route path={ROUTES.Home} element={<HomePage />}/>
-        <Route path={ROUTES.Product} element={<Products/>}/>
-        <Route path={ROUTES.Item} element={<ItemDetail/>}/>
-        <Route path={ROUTES.About} element={<About/>}/>
-        <Route path={ROUTES.ContactUs} element={<ContactUs/>}/>
-      </Routes>
-      </Router>
+    // <ThemeProvider theme={theme}>
+    //   <Router>
+    //     <Routes>
+    //       <Route path={ROUTES.LANDING} element={<Home />} >
+    //         <Route path={ROUTES.HOME} element={<Home />} />
+    //       </Route>
+    //       <Route path={ROUTES.SIGN_IN} element={<SignIn />} />
+    //       <Route path={ROUTES.SIGN_UP} element={<SignUp />} />
+    //       <Route path={ROUTES.Users} element={<Users />} />
+
+    //     </Routes>
+    //   </Router>
+    // </ThemeProvider>
+    // <Login />
+    <>
+    {/* <Profile  /> */}
+
+    <AddToCart />
+    <div>
+      <h1>Search Example</h1>
+      <SearchComponent data={allData} onSearch={handleSearch} />
+      
+      {/* Render search results */}
+      <ul>
+        {searchResults.map((item, index) => (
+          <li key={index}>{item.name}</li>
+        ))}
+      </ul>
+    </div>
+    <Reviews/>
     </>
+    
   );
-};
+}
 
 export default App;

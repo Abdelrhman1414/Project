cc57a5017b196a064b722f3d473c72595e4b9787
Mohamedyasser02 <mohammedyasser0031@gmail.com>
2023-12-08 03:20:37 +0200

    added cartModel

---
+++ Backend/models/cartmodel.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const { schema } = require('./productmodel');
+const Schema = mongoose.Schema;
+
+const cartSchema = new schema({
+    products : [
+        {
+        product: {
+            type : mongoose.Schema.Types.ObjectId,
+            ref : "Product"
+        },
+        count : Number,
+        price : Number,
+    },
+    ],
+    cartPrice: Number,
+    orderby : {
+        type: mongoose.Schema.Types.ObjectId,
+        ref : "User",
+    }
+    
+
+},{timestamps:true});
+
+
+module.exports = mongoose.model("Cart",cartSchema);

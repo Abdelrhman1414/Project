56300362133edd5dd6ef655d4c8b872ce3739e8e
Mohamedyasser02 <mohammedyasser0031@gmail.com>
2023-12-08 22:02:51 +0200

    last

--- Backend/config/dbConnect.js
+++ Backend/config/dbConnect.js
@@ -5,8 +5,9 @@ try{
     const conn=mongoose.connect("mongodb://localhost:27017/lab3db");
 console.log("Database connected Succesfully")
 }
+
 catch(error){
-   console.log(" Data Error")
+console.log(" Data Error")
 }
 }
 module.exports= dbConnect;

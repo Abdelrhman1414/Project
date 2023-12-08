03ba174abe339ba1edf962159e193a527fa89ec2
Aminnadel <121466688+Aminnadel@users.noreply.github.com>
2023-12-08 22:53:44 +0200

    Add files via upload

---
+++ frontend/src/component/UserComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const UserComponent = () => {
+  return (
+    <div>
+      <h2>User Dashboard</h2>
+      <p>You do not have the privilege to add items.</p>
+    </div>
+  );
+};
+
+export default UserComponent;

d4b79dbe0571a1873bb0c6f8a6faffdbef8b25f6
Mohamedyasser02 <mohammedyasser0031@gmail.com>
2023-12-08 16:00:41 +0200

    check email pattern by abdelrahman

--- Backend/controller/userCtrl.js
+++ Backend/controller/userCtrl.js
@@ -4,24 +4,33 @@ const asyncHandler=require('express-async-handler')
 
 
 const createUser= asyncHandler (async(req,res)=>{
-const email=req.body.email;
-const findUser=await User.findOne({email:email});
-if (!findUser) {
-
-    const newUser = await User.create(req.body);
-    res.json(newUser);
-  } else {
-    
-    throw new Error("User Already Exists");
+  const email=req.body.email;
+  
+  if (!isValidEmail(email)) {
+    return res.status(400).json({ error: 'Invalid email format' });
   }
-}); 
+  
+  else{
+  
+  
+  const findUser=await User.findOne({email:email});
+  if (!findUser) {
+  
+      const newUser = await User.create(req.body);
+      res.json(newUser);
+    } else {
+  
+      throw new Error("User Already Exists");
+    }
+  }
+  
+  }); 
 
 const loginUserCtrl = asyncHandler(async (req, res) => {
     const { email, password } = req.body;
 
     const findUser = await User.findOne({ email });
     if (findUser && (await findUser.isPasswordMatched(password))) {
-   
       res.json({
         _id: findUser?._id,
         firstname: findUser?.firstname,
@@ -97,6 +106,11 @@ const loginUserCtrl = asyncHandler(async (req, res) => {
       throw new Error(error);
     }
   });
+
+  function isValidEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+.[^\s@]+$/;
+    return emailRegex.test(email);
+  }
 
 
 
